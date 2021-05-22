357aa0842d57f8a9ce110b4dcdf01ea9684cd460
Lluis Mendoza <lluis.mendoza.vandrell1030@gmail.com>
2021-05-22 14:55:54 +0200

    heroku deploy

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'port': process.env.PORT||5000,
-    'database': 'mongodb+srv://lluis:achanta1030@cluster0.phgxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    'database': 'mongodb+srv://lluis:achanta1030@cluster0.phgxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' 
 };
